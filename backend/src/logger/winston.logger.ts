b54d43b7015e8352f83479638db2eda3a6c6a3ec
smw0807 <smw0807@gmail.com>
2024-03-17 19:22:01 +0900

    feat: 로그파일 생성 추가

--- backend/src/logger/winston.logger.ts
+++ backend/src/logger/winston.logger.ts
@@ -1,5 +1,6 @@
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
 
 export default () => ({
   transports: [
@@ -8,6 +9,32 @@ export default () => ({
       format: winston.format.combine(
         winston.format.timestamp(),
         nestWinstonModuleUtilities.format.nestLike(process.env.PROJECT_NAME),
+      ),
+    }),
+    // 일반 로그 파일 설정 (날짜별 로테이션)
+    new DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    // 오류 로그 파일 설정 (날짜별 로테이션)
+    new DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
       ),
     }),
   ],
