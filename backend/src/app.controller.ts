36b8277306178c951685d097822c5095c03d1621
smw0807 <smw0807@gmail.com>
2024-03-17 19:13:47 +0900

    feat: 커스텀 로거 테스트

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('log-test')
+  getLogTest(): string {
+    this.logger.log('This is a test log');
+    this.logger.debug('This is a test debug');
+    this.logger.error('This is a test error');
+    this.logger.verbose('This is a test verbose');
+    this.logger.warn('This is a test warn');
+    return 'Log test';
   }
 }
