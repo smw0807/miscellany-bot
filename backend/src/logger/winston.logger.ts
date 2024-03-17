344faf29342cfe9c88b1f3a004e9baafe5513ad2
smw0807 <smw0807@gmail.com>
2024-03-17 19:13:33 +0900

    feat: 커스텀 윈스턴 로거 생성 및 적용

---
+++ backend/src/logger/winston.logger.ts
@@ -0,0 +1,14 @@
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import * as winston from 'winston';
+
+export default () => ({
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        nestWinstonModuleUtilities.format.nestLike(process.env.PROJECT_NAME),
+      ),
+    }),
+  ],
+});
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import winstonLogger from './logger/winston.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(WinstonModule.createLogger(winstonLogger()));
   await app.listen(3000);
 }
 bootstrap();
