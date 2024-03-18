4f8af8b277657414c133b0952620460b86b6a5bf
송민우 <smw0807@gmail.com>
2024-03-18 17:13:23 +0900

    feat: 환경변수 체크에 NODE_ENV 추가

--- backend/src/config/conf/common.config.ts
+++ backend/src/config/conf/common.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('common', () => ({
+  NODE_ENV: process.env.NODE_ENV,
   projectName: process.env.PROJECT_NAME,
   appPort: process.env.APP_PORT,
 }));
--- backend/src/config/validation.schema.ts
+++ backend/src/config/validation.schema.ts
@@ -1,6 +1,9 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production')
+    .default('development'),
   PROJECT_NAME: Joi.string().required(),
   APP_PORT: Joi.number().default(3000),
 });
