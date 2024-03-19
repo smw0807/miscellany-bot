8407fb23b2535a311fd47723ab760673f775684f
송민우 <smw0807@gmail.com>
2024-03-19 17:28:16 +0900

    refactor: 로거 수정

--- backend/src/logger/winston.logger.ts
+++ backend/src/logger/winston.logger.ts
@@ -1,8 +1,11 @@
-import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
 import * as winston from 'winston';
 import * as DailyRotateFile from 'winston-daily-rotate-file';
 
-export default () => ({
+export const winstonLogger = WinstonModule.createLogger({
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WinstonModule } from 'nest-winston';
-import winstonLogger from './logger/winston.logger';
+import { winstonLogger } from './logger/winston.logger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useLogger(WinstonModule.createLogger(winstonLogger()));
+  app.useLogger(winstonLogger);
 
   const configService = app.get(ConfigService);
   const port = configService.get('common.appPort');
