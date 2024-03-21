98e9c5ad84b4d21091eb281344402a57d61e8302
smw0807 <smw0807@gmail.com>
2024-03-21 22:49:27 +0900

    delete: 테스트 로직 제거 메시지 생성 이벤트 리스너 옮기고 테스트 코드 제거

--- backend/src/discord/messages/discord.trigger.service.ts
+++ backend/src/discord/messages/discord.trigger.service.ts
@@ -7,20 +7,5 @@ import { DiscordClientService } from '../discord.client.service';
 export class DiscordMessageTriggerService {
   private readonly logger = new Logger(DiscordMessageTriggerService.name);
 
-  constructor(private readonly clinet: DiscordClientService) {
-    this.pingPongTest();
-  }
-
-  /**
-   * 테스트용 메시지 트리거
-   * 디스코드 메시지가 'ping'일 경우 'pong'으로 응답
-   */
-  pingPongTest() {
-    this.clinet.clientInstance.on('messageCreate', (msg) => {
-      this.logger.debug('messageCreate', msg);
-      if (msg.content === 'ping') {
-        msg.reply('pong');
-      }
-    });
-  }
+  constructor(private readonly clinet: DiscordClientService) {}
 }
