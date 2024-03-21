d80083786bf18ebe73b196712345ac4469dfce9f
smw0807 <smw0807@gmail.com>
2024-03-21 22:48:37 +0900

    add: 상수 값 저장 디렉터리 추가 및 언어 코드, 커맨드 정의 파일 추가

---
+++ backend/src/constants/language-codes.ts
@@ -0,0 +1,8 @@
+// 언어 코드 정의 파일
+
+export enum LanguageCode {
+  German = 'de',
+  Japanese = 'ja',
+  English = 'en',
+  Korean = 'ko',
+}
---
+++ backend/src/constants/language-commands.ts
@@ -0,0 +1,7 @@
+// 디스코드 언어 번역 커맨드
+export enum LanguageCommand {
+  TranslageToGerman = 'Translate to German',
+  TranslageToJapanese = 'Translate to Japanese',
+  TranslageToEnglish = 'Translate to English',
+  TranslageToKorean = 'Translate to Korean',
+}
