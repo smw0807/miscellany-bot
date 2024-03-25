71e3f7d2c54c0638a4f2445fe16283dc4e5159b4
smw0807 <smw0807@gmail.com>
2024-03-25 21:55:01 +0900

    refactor: 컨텍스트메뉴 로직 수정 나중에 커맨드로 번역말고 다른 것도 추가할 수 있을 것 같아서 이벤트리스너 안에 있는 번역 로직을 함수로 분리 번역 관련 명령일 때 번역 함수를 실행하도로 변경

--- backend/src/discord/commands/discord.contextMenu.service.ts
+++ backend/src/discord/commands/discord.contextMenu.service.ts
@@ -1,7 +1,12 @@
 // 디스코드 컨텍스트메뉴 서비스
 
 import { Injectable, Logger } from '@nestjs/common';
-import { ApplicationCommandType, Client, EmbedBuilder } from 'discord.js';
+import {
+  ApplicationCommandType,
+  Client,
+  EmbedBuilder,
+  MessageContextMenuCommandInteraction,
+} from 'discord.js';
 import { GoogleTranslateService } from 'src/google-translate/google.translate.service';
 import { LanguageCode } from 'src/constants/language-codes';
 import { LanguageCommand } from 'src/constants/language-commands';
@@ -69,36 +74,51 @@ export class DiscordContextMenuService {
     client.on('interactionCreate', async (interaction) => {
       if (!interaction.isMessageContextMenuCommand()) return;
 
-      // 선택한 커맨드
-      const targetLanguage = this.getTargetLanguage(interaction.commandName);
+      // 선택한 커맨드 명령이름이 번역과 관련 있는지 확인
+      const languageCommands = Object.values(LanguageCommand);
+      if (
+        languageCommands.includes(interaction.commandName as LanguageCommand)
+      ) {
+        await this.trnaslateCommand(interaction);
+      }
 
-      // 선택한 메시지 정보
-      const message = await interaction.channel.messages.fetch(
-        interaction.targetId,
-      );
-
-      // 입력 메시지
-      const content = message.content;
-      // 텍스트 번역
-      const translateText = await this.translateService.translateText(
-        content,
-        targetLanguage,
-      );
-
-      // Embed 메시지 생성
-      const embed = new EmbedBuilder()
-        .setColor(0x009999)
-        .setTitle(
-          content.length <= 10 ? content : `${content.substring(0, 20)}...`,
-        )
-        .setDescription(translateText)
-        .setURL(message.url)
-        .setTimestamp()
-        .setFooter({ text: 'Powered by Google Translate' }); // Google Translate로 번역된 것임을 표시
-
-      // 번역 결과 메시지 전송
-      await interaction.reply({ embeds: [embed] });
+      if (interaction.commandName === 'TEST') {
+        this.logger.debug('### [ TEST ] ###');
+      }
     });
+  }
+
+  // 번역 커맨드 실행
+  async trnaslateCommand(interaction: MessageContextMenuCommandInteraction) {
+    // 선택한 커맨드
+    const targetLanguage = this.getTargetLanguage(interaction.commandName);
+
+    // 선택한 메시지 정보
+    const message = await interaction.channel.messages.fetch(
+      interaction.targetId,
+    );
+
+    // 입력 메시지
+    const content = message.content;
+    // 텍스트 번역
+    const translateText = await this.translateService.translateText(
+      content,
+      targetLanguage,
+    );
+
+    // Embed 메시지 생성
+    const embed = new EmbedBuilder()
+      .setColor(0x009999)
+      .setTitle(
+        content.length <= 10 ? content : `${content.substring(0, 20)}...`,
+      )
+      .setDescription(translateText)
+      .setURL(message.url)
+      .setTimestamp()
+      .setFooter({ text: 'Powered by Google Translate' }); // Google Translate로 번역된 것임을 표시
+
+    // 번역 결과 메시지 전송
+    await interaction.reply({ embeds: [embed] });
   }
 
   // 선택한 번역 언어 코드
