172ee2d809fb1eb58ec918cb063fd8dddf074ac6
smw0807 <smw0807@gmail.com>
2024-03-30 11:11:56 +0900

    add: supabase config

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -4,6 +4,7 @@ import commonConfig from './conf/common.config';
 import { validationSchema } from './validation.schema';
 import discordConfig from './conf/discord.config';
 import googleTranslateConfig from './conf/google.translate.config';
+import supabaseConfig from './conf/supabase.config';
 
 const developmentEnv = '.env.development';
 const productionEnv = '.env';
@@ -12,7 +13,12 @@ const productionEnv = '.env';
     NestConfigModule.forRoot({
       envFilePath:
         process.env.NODE_ENV === 'development' ? developmentEnv : productionEnv,
-      load: [commonConfig, discordConfig, googleTranslateConfig],
+      load: [
+        commonConfig,
+        discordConfig,
+        googleTranslateConfig,
+        supabaseConfig,
+      ],
       isGlobal: true,
       validationSchema,
     }),
