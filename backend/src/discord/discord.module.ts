3961fd015096c5337db81d6d70d89ea43a7d6a43
smw0807 <smw0807@gmail.com>
2024-03-30 13:04:33 +0900

    remove: 삭제한 프로바이더 import 제거

--- backend/src/discord/discord.module.ts
+++ backend/src/discord/discord.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { DiscordClientService } from './client/discord.client.service';
-import { DiscordMessageTriggerService } from './messages/discord.trigger.service';
-import { DiscordReservationMessageService } from './messages/discord.reservation.service';
 import { GoogleTranslateModule } from 'src/google-translate/google.translate.module';
 import { DiscordService } from './discord.service';
 import { DiscordContextMenuService } from './commands/discord.contextMenu.service';
@@ -12,8 +10,6 @@ import { DiscordMessageService } from './messages/discord.message.service';
     DiscordClientService,
     DiscordContextMenuService,
     DiscordMessageService,
-    DiscordMessageTriggerService,
-    DiscordReservationMessageService,
     DiscordService,
   ],
   imports: [GoogleTranslateModule],
