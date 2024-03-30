667e4e53d107ed8946796c8cc1af4159f398ab1d
smw0807 <smw0807@gmail.com>
2024-03-30 12:59:30 +0900

    remove: 구현 불확실해서 삭제 나중에 구현할 것 같을 때 다시 만들예정

--- backend/src/discord/messages/discord.reservation.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable, Logger } from '@nestjs/common';
-// 디스코드 예약 메시지 서비스
-@Injectable()
-export class DiscordReservationMessageService {
-  private readonly logger = new Logger(DiscordReservationMessageService.name);
-}
