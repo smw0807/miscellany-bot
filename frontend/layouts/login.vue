a0b09201bc4ad3edafda34107dfefbd77027ca83
smw0807 <smw0807@gmail.com>
2024-04-01 23:32:58 +0900

    로그인 화면 디자인 잡는 중...

---
+++ frontend/layouts/login.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <v-app>
+    <v-main class="d-flex align-center justify-center">
+      <slot />
+    </v-main>
+  </v-app>
+</template>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'login',
+});
+
+const login = () => {
+  console.log('login');
+};
+</script>
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>Login</v-card-title>
+          <v-card-action>
+            <v-btn color="primary" @click="login"
+              >디스코드 계정으로 로그인하기</v-btn
+            >
+          </v-card-action>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
