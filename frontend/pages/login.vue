d36f4b09c7107f35b364a97422416df06b2f49a2
smw0807 <smw0807@gmail.com>
2024-04-01 23:34:11 +0900

    중앙 정렬

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -10,14 +10,14 @@ const login = () => {
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col class="text-center">
         <v-card>
           <v-card-title>Login</v-card-title>
-          <v-card-action>
+          <v-card-text>
             <v-btn color="primary" @click="login"
               >디스코드 계정으로 로그인하기</v-btn
             >
-          </v-card-action>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
