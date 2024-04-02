4175be9c32856c6be7203d5789a0c228dba2abd9
smw0807 <smw0807@gmail.com>
2024-04-02 13:35:40 +0900

    feat: prefix 추가 (api)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(winstonLogger);
 
+  // api prefix 적용
+  app.setGlobalPrefix('api');
+
   const configService = app.get(ConfigService);
   const port = configService.get('common.appPort');
   await app.listen(port);
