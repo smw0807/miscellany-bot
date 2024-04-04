85e34ac46c8ff967cec4c4f61c534ad743d08374
smw0807 <smw0807@gmail.com>
2024-04-04 12:12:16 +0900

    feat: cors 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,13 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
+  // cors 설정
+  const corsConfig = configService.get('cors');
+  app.enableCors({
+    origin: corsConfig.origin,
+    methods: corsConfig.methods,
+    allowedHeaders: corsConfig.allowedHeaders,
+  });
   const port = configService.get('common.appPort');
   await app.listen(port);
 }
