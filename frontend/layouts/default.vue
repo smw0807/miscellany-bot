c248dd753ce123386f01a19e91f5f0f0a044e480
smw0807 <smw0807@gmail.com>
2024-04-05 13:00:22 +0900

    todo: 로그인 페이지 이동 로직

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const router = useRouter();
+// TODO 디스코드 로그인 정보 있는지 체크 후 없으면 login 페이지로 이동하게 해야함
+// router.push('/login');
+</script>
 <template>
   <slot />
 </template>
