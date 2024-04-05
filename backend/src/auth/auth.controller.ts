5d88d240934432f777ba8eaea1d44f9f7da8cca4
smw0807 <smw0807@gmail.com>
2024-04-05 13:02:16 +0900

    feat: auth/discord-login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,26 +1,15 @@
-import { Controller, Get, Inject, Logger, Res } from '@nestjs/common';
+import { Controller, Get, Logger, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { ConfigType } from '@nestjs/config';
-import DiscordConfig from 'src/config/conf/discord.config';
-import SupabaseConfig from 'src/config/conf/supabase.config';
+import { SupabaseService } from 'src/supabase/supabase.service';
 
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger(AuthController.name);
-  constructor(
-    @Inject(DiscordConfig.KEY)
-    private readonly discordConfig: ConfigType<typeof DiscordConfig>,
-    @Inject(SupabaseConfig.KEY)
-    private readonly supabaseConfig: ConfigType<typeof SupabaseConfig>,
-  ) {}
+  constructor(private readonly supabaseService: SupabaseService) {}
 
   @Get('discord-login')
-  redirectToDiscord(@Res() res: Response) {
-    this.logger.log('Redirecting to Discord login');
-    const discordClientID = this.discordConfig.dicordClientID;
-    const authLoginUrl = this.discordConfig.discordRedirectUrl;
-    const discordLoginUrl = `https://discord.com/api/oauth2/authorize?client_id=${discordClientID}&redirect_uri=${authLoginUrl}&response_type=code`;
-
-    res.redirect(discordLoginUrl);
+  async redirectToDiscord(@Res() res: Response) {
+    const signinUrl = await this.supabaseService.signInWithDiscord();
+    res.send(signinUrl);
   }
 }
