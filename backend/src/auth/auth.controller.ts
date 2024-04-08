65491b0bc5a7fca66ccaacf2e57fd86ba6ed580b
smw0807 <smw0807@gmail.com>
2024-04-08 13:53:37 +0900

    feat: 디스코드 로그인 URL, 토큰 발급 엔드포인트

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,53 @@
-import { Controller, Get, Logger, Res } from '@nestjs/common';
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  Logger,
+  Post,
+  Query,
+  Res,
+  Session,
+} from '@nestjs/common';
 import { Response } from 'express';
-import { SupabaseService } from 'src/supabase/supabase.service';
+import { DiscordAuthService } from 'src/discord/auth/discord.auth.service';
 
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger(AuthController.name);
-  constructor(private readonly supabaseService: SupabaseService) {}
+  constructor(private readonly discordAuthService: DiscordAuthService) {}
 
-  @Get('discord-login')
-  async redirectToDiscord(@Res() res: Response) {
-    const signinUrl = await this.supabaseService.signInWithDiscord();
-    res.send(signinUrl);
+  // 디스코드 로그인 URL 생성
+  @Get('discord/login')
+  async redirectToDiscord(
+    @Res() res: Response,
+    @Session() session: Record<string, any>,
+  ) {
+    try {
+      const signinUrl = this.discordAuthService.redirectToDiscord(session);
+      res.send(signinUrl);
+    } catch (e) {
+      this.logger.error(e.message);
+      res.status(400).send(e.message);
+    }
+  }
+
+  // 디스코드 토큰 요청
+  @Post('discord/token')
+  async getDiscordAccessToken(
+    @Res() res: Response,
+    @Session() session: Record<string, any>,
+    @Query('code') code: string,
+    @Query('state') state: string,
+  ) {
+    try {
+      const accessToken = await this.discordAuthService.requestDiscordToken(
+        session,
+        code,
+        state,
+      );
+      res.send(accessToken);
+    } catch (e) {
+      res.status(403).send(new ForbiddenException('Invalid state'));
+    }
   }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { SupabaseModule } from 'src/supabase/supabase.module';
+import { DiscordModule } from 'src/discord/discord.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [DiscordModule],
   controllers: [AuthController],
 })
 export class AuthModule {}
