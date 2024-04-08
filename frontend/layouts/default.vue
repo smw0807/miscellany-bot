24838d9fffd1f0d58f609854ed689ac837e46e6e
smw0807 <smw0807@gmail.com>
2024-04-08 23:16:43 +0900

    feat: 쿠키에 토큰 체크 로직 추가 없으면 로그인 화면으로 이동

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-const router = useRouter();
-// TODO 디스코드 로그인 정보 있는지 체크 후 없으면 login 페이지로 이동하게 해야함
-// router.push('/login');
+const { hasToken } = useAuth();
+// 토큰이 없을 경우 로그인
+if (!hasToken()) {
+  const router = useRouter();
+  console.warn('로그인이 필요합니다.');
+  router.push('/login');
+}
 </script>
 <template>
   <slot />
