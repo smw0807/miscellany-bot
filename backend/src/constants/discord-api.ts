8616aea51ea7b420e15e6d02e0f1947aaa9922c6
smw0807 <smw0807@gmail.com>
2024-04-11 13:13:03 +0900

    rename: 파일명 변경

--- backend/src/constants/discord-api.ts
+++ backend/src/constants/discord-api.ts
@@ -1,4 +1,5 @@
-//디스코드 API URL
+//디스코드 API에 필요한 타입 정의 파일
+
 const API_URL = 'https://discord.com/api';
 
 //디스코드 API URL 리스트
@@ -12,3 +13,11 @@ export const DISCORD_API_URL = {
   // 유저 정보(본인-email)
   USER: `${API_URL}/users/@me`,
 };
+
+// 디스코드 API grant_type
+export enum DISCORD_GRANT_TYPE {
+  AUTHORIZATION_CODE = 'authorization_code',
+  CLIENT_CREDENTIALS = 'client_credentials',
+  ACCESS_TOKEN = 'access_token',
+  REFRESH_TOKEN = 'refresh_token',
+}
