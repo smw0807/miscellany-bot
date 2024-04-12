6231954a41fd1a5b3889df0815b5dfd7c84ac237
smw0807 <smw0807@gmail.com>
2024-04-12 21:39:16 +0900

    feat: 관리중인 길드 리스트 API 요청 추가

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 //pinia test
 import { useStore } from '@/store';
 
 const store = useStore();
+
+// 관리중인 길드 리스트 조회 요청
+const getList = async () => {
+  try {
+    const config = useRuntimeConfig();
+    const token = useCookie(config.public.accessTokenName);
+
+    const result = await $fetch('/api/discord/guilds', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+    console.log(result);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+onMounted(() => {
+  getList();
+});
 </script>
 <template>
   <v-col> test : {{ store.count }}</v-col>
