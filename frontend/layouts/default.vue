b14687e3d7797952df2d6d246ab74bc0ad359152
smw0807 <smw0807@gmail.com>
2024-04-13 16:53:31 +0900

    refactor: 서버 목록 요청 위치 변경 서버 목록은 항상 있어야할 것 같아서 layouts단에서 요청하도록 변경

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useDiscordStore, type DiscordGuildsType } from '~/store/discord';
+
 const { hasToken } = useAuth();
 // 토큰이 없을 경우 로그인
 if (!hasToken()) {
@@ -6,6 +8,18 @@ if (!hasToken()) {
   console.warn('로그인이 필요합니다.');
   router.push('/login');
 }
+
+const discordStore = useDiscordStore();
+// 관리중인 서버 목록 조회 요청
+const getGuilds = async () => {
+  await discordStore.requestGuilds();
+};
+
+onMounted(async () => {
+  if (discordStore.guilds.length === 0) {
+    await getGuilds();
+  }
+});
 </script>
 <template>
   <slot />
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { useDiscordStore, type DiscordGuildsType } from '~/store/discord';
 
 const discordStore = useDiscordStore();
 
-// 관리중인 서버 목록 조회 요청
-const getGuilds = async () => {
-  await discordStore.requestGuilds();
-};
 // 관리중인 서버 목록
 const cGuilds = computed<DiscordGuildsType[]>(() => discordStore.guilds);
-
-onMounted(async () => {
-  if (discordStore.guilds.length === 0) {
-    await getGuilds();
-  }
-});
 </script>
 <template>
   <v-col> {{ cGuilds }}</v-col>
