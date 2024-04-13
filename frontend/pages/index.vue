7035e027c85ad94c87d1c681afc6de079bbdbb3d
smw0807 <smw0807@gmail.com>
2024-04-13 16:34:14 +0900

    refactor: 서버 목록 조회 스토어로 처리하게 변경

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,32 +1,22 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-//pinia test
-import { useStore } from '@/store';
+import { useDiscordStore, type DiscordGuildsType } from '~/store/discord';
 
-const store = useStore();
+const discordStore = useDiscordStore();
 
-// 관리중인 길드 리스트 조회 요청
-const getList = async () => {
-  try {
-    const config = useRuntimeConfig();
-    const token = useCookie(config.public.accessTokenName);
-
-    const result = await $fetch('/api/discord/guilds', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token.value}`,
-      },
-    });
-    console.log(result);
-  } catch (e) {
-    console.error(e);
-  }
+// 관리중인 서버 목록 조회 요청
+const getGuilds = async () => {
+  await discordStore.requestGuilds();
 };
+// 관리중인 서버 목록
+const cGuilds = computed<DiscordGuildsType[]>(() => discordStore.guilds);
 
-onMounted(() => {
-  getList();
+onMounted(async () => {
+  if (discordStore.guilds.length === 0) {
+    await getGuilds();
+  }
 });
 </script>
 <template>
-  <v-col> test : {{ store.count }}</v-col>
+  <v-col> {{ cGuilds }}</v-col>
 </template>
