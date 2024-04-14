fbf4e1cc405d8bca67d71de3e2ba3eb020b43092
smw0807 <smw0807@gmail.com>
2024-04-14 19:14:51 +0900

    refactor: 서버 목록 조회 조건문 수정

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,24 +2,24 @@
 import { useDiscordStore, type DiscordGuildsType } from '~/store/discord';
 
 const { hasToken } = useAuth();
-// 토큰이 없을 경우 로그인
-if (!hasToken()) {
+if (hasToken()) {
+  const discordStore = useDiscordStore();
+  // 관리중인 서버 목록 조회 요청
+  const getGuilds = async () => {
+    await discordStore.requestGuilds();
+  };
+
+  onMounted(async () => {
+    if (discordStore.guilds.length === 0) {
+      await getGuilds();
+    }
+  });
+} else {
+  // 토큰이 없을 경우 로그인 페이지로 이동
   const router = useRouter();
   console.warn('로그인이 필요합니다.');
   router.push('/login');
 }
-
-const discordStore = useDiscordStore();
-// 관리중인 서버 목록 조회 요청
-const getGuilds = async () => {
-  await discordStore.requestGuilds();
-};
-
-onMounted(async () => {
-  if (discordStore.guilds.length === 0) {
-    await getGuilds();
-  }
-});
 </script>
 <template>
   <slot />
