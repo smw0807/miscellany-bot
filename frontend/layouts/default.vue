718b73da3de28c93ab539e88392789393cb9f42f
smw0807 <smw0807@gmail.com>
2024-04-18 20:46:23 +0900

    feat: 관리 페이지일 때 앱바에 홈으로 버튼 추가

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,6 +4,8 @@ import { useDiscordStore, type DiscordGuildsType } from '~/store/discord';
 const config = useRuntimeConfig();
 const projectName: Ref<string> = ref(config.public.projectName);
 
+const router = useRouter();
+
 const { hasToken } = useAuth();
 if (hasToken()) {
   const discordStore = useDiscordStore();
@@ -19,10 +21,13 @@ if (hasToken()) {
   });
 } else {
   // 토큰이 없을 경우 로그인 페이지로 이동
-  const router = useRouter();
+
   console.warn('로그인이 필요합니다.');
   router.push('/login');
 }
+
+// 현재 페이지 이름
+const currentPageName = computed(() => router.currentRoute.value.name);
 </script>
 <template>
   <v-layout class="rounded rounded-md">
@@ -31,6 +36,18 @@ if (hasToken()) {
         <v-img src="/logo.png" alt="logo" />
       </v-avatar>
       <span class="text-h5">{{ projectName }}</span>
+      <v-spacer />
+
+      <!-- 관리 화면일 때만 활성화 -->
+      <template v-slot:append>
+        <v-btn
+          v-if="currentPageName === 'manage'"
+          prepend-icon="mdi-home"
+          @click="router.push('/')"
+        >
+          홈으로
+        </v-btn>
+      </template>
     </v-app-bar>
 
     <v-main class="d-flex align-center justify-center">
