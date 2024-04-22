ab6674675ca304924bb0125dcb2386e9d5027fc1
smw0807 <smw0807@gmail.com>
2024-04-22 22:32:40 +0900

    feat: 서버정보 로컬스토리지 컨트롤하는 컴포지션 추가

---
+++ frontend/composables/useGuild.ts
@@ -0,0 +1,18 @@
+import type { DiscordGuildsType } from '~/store/discord';
+
+/**
+ * 로컬 스토리지에 길드 정보를 저장하고 불러오는 컴포지션
+ * @returns
+ */
+export default function () {
+  return {
+    // 길드 정보 저장
+    saveGuild(storageName: string, guild: DiscordGuildsType) {
+      localStorage.setItem(storageName, JSON.stringify(guild));
+    },
+    // 길드 정보 불러오기
+    loadGuild(storageName: string): DiscordGuildsType {
+      return JSON.parse(localStorage.getItem(storageName) || '{}');
+    },
+  };
+}
