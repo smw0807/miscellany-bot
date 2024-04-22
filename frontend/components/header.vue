efbf332d57b25a588775a10501c2ddf48b498f08
smw0807 <smw0807@gmail.com>
2024-04-22 21:22:41 +0900

    refactor: app-bar부분 Header 컴포넌트로 분리

---
+++ frontend/components/header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const projectName: Ref<string> = ref(config.public.projectName);
+
+const router = useRouter();
+// 현재 페이지 이름
+const currentPageName = computed(() => router.currentRoute.value.name);
+</script>
+<template>
+  <v-app-bar>
+    <v-avatar class="mx-3">
+      <v-img src="/logo.png" alt="logo" />
+    </v-avatar>
+    <span class="text-h5">{{ projectName }}</span>
+    <v-spacer />
+
+    <!-- 관리 화면일 때만 활성화 -->
+    <template v-slot:append>
+      <v-btn
+        v-if="currentPageName === 'manage'"
+        prepend-icon="mdi-home"
+        @click="router.push('/')"
+      >
+        홈으로
+      </v-btn>
+    </template>
+  </v-app-bar>
+</template>
