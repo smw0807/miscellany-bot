e00da6375fd5d8a2bf19751301b52af83857a9e8
smw0807 <smw0807@gmail.com>
2024-05-02 21:59:04 +0900

    extends로 쓰게끔 로직변경

--- backend/src/discord/client/discord.client.service.ts
+++ backend/src/discord/client/discord.client.service.ts
@@ -1,21 +1,22 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import discordConfig from 'src/config/conf/discord.config';
 import { Client, GatewayIntentBits } from 'discord.js';
 
 @Injectable()
 export class DiscordClientService {
-  private readonly logger = new Logger(DiscordClientService.name);
   // 봇 클라이언트
-  private client: Client;
+  private clientInstance: Client;
   constructor(
     @Inject(discordConfig.KEY) private config: ConfigType<typeof discordConfig>,
-  ) {}
+  ) {
+    this.setupDiscordBot();
+  }
 
   // 디스코드 봇 클라이언트 생성 및 로그인
   async setupDiscordBot() {
     // 봇 클라이언트 생성
-    this.client = new Client({
+    this.clientInstance = new Client({
       intents: [
         GatewayIntentBits.Guilds, // 서버 목록 조회
         GatewayIntentBits.GuildMessages, // 서버 메시지 조회
@@ -26,7 +27,7 @@ export class DiscordClientService {
     await this.client.login(this.config.discordToken);
   }
 
-  get clientInstance() {
-    return this.client;
+  get client() {
+    return this.clientInstance;
   }
 }
