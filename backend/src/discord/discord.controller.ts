b0250cfb066adcaf72ac0f548a98ed740fc5771f
smw0807 <smw0807@gmail.com>
2024-05-05 17:57:55 +0900

    todo: 채널에 메시지 보내는 함수 인식 안되는 원인 찾기

--- backend/src/discord/discord.controller.ts
+++ backend/src/discord/discord.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Logger, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { DiscordMessageService } from './messages/discord.message.service';
 import { DiscordGuildsService } from './\bguilds/discord.guilds.service';
+import { SendMessageType } from './types/messages';
 
 @Controller('discord')
 export class DiscordController {
@@ -28,15 +29,20 @@ export class DiscordController {
   }
 
   // 디스코드 채널로 메시지 보내기
+  // todo channel.send를 못찾는다고 떠서 해결 방법 찾아야함
   @Get('send-message')
   async sendMessage(@Req() req: Request, @Res() res: Response) {
     try {
-      const accessToken = req.headers.authorization;
-      const { channelId, message, isEveryone } = req.query;
-      console.log(accessToken);
-      console.log(channelId);
-      console.log(message);
-      console.log(isEveryone);
+      // const accessToken = req.headers.authorization;
+      const { guildId, channelId, message, isEveryone } = req.query;
+      const data: SendMessageType = {
+        guildId: guildId as string,
+        channelId: channelId as string,
+        message: message as string,
+        isEveryone: Boolean(isEveryone),
+      };
+      const result = await this.messageService.sendMessage(data);
+      console.log('result :', result);
       res.send('test');
     } catch (e) {
       res.status(500).send(e.message);
--- backend/src/discord/messages/discord.message.service.ts
+++ backend/src/discord/messages/discord.message.service.ts
@@ -1,11 +1,19 @@
 // 디스코드 메시지 서비스
-import { Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Client } from 'discord.js';
+import { DiscordClientService } from '../client/discord.client.service';
+import discordConfig from 'src/config/conf/discord.config';
+import { ConfigType } from '@nestjs/config';
+import { SendMessageType } from '../types/messages';
 
 @Injectable()
-export class DiscordMessageService {
+export class DiscordMessageService extends DiscordClientService {
   private readonly logger = new Logger(DiscordMessageService.name);
-  constructor() {}
+  constructor(
+    @Inject(discordConfig.KEY) config: ConfigType<typeof discordConfig>,
+  ) {
+    super(config);
+  }
 
   // 메시지 이벤트 리스너
   onMessage(client: Client) {
@@ -19,4 +27,27 @@ export class DiscordMessageService {
       this.logger.log(logMessage);
     });
   }
+
+  // 메시지 보내기
+  async sendMessage(data: SendMessageType) {
+    try {
+      console.log('sendMessage : ', data);
+      // const { guildId, channelId, message } = data;
+      // const guild = this.client.guilds.cache.get(guildId);
+      // console.log(guild);
+      const channel = this.client.channels.cache.get('10982356155747697112');
+      if (!channel) {
+        throw new Error('채널을 찾을 수 없습니다.');
+      }
+      // todo send를 못찾는다고 떠서 해결 방법 찾아야함
+      // await channel.send(message, {
+      //   allowedMentions: { parse: ['users', 'roles', 'everyone'] },
+      // });
+
+      return false;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
+// 1098235615574769711
