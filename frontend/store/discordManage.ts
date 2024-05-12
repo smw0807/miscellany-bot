6b0b5d0c2dca48fd4fce7556d533cf92bb542e6d
smw0807 <smw0807@gmail.com>
2024-05-12 22:25:50 +0900

    채널 및 메시지보내기 타입 추가 및 메시지보내기 api 요청 로직 수정

--- frontend/store/discordManage.ts
+++ frontend/store/discordManage.ts
@@ -1,35 +1,47 @@
+export type ChannelType = {
+  id: string;
+  name: string;
+  type: string;
+};
+export type SendMessageType = {
+  guildId: string;
+  channelId: string;
+  message: string;
+  isEveryone: boolean;
+};
+
 export const useDiscordManageStore = defineStore('discordManage', () => {
   // ============= State =============
-  const state = {};
-
-  // ============= Getters =============
-  const getters = {};
+  //서버에 있는 채널 목록
+  const channelList = ref<ChannelType[]>([]);
+  const state = { channelList };
 
   // ============= Actions =============
-  // 채널로 메시지 보내기
-  // todo channel.send를 못찾는다고 떠서 해결되어야 완성 가능
-  const sendMessage = async (
-    guildId: string,
-    channelId: string,
-    message: string,
-    isEveryone: boolean = false
-  ): Promise<boolean> => {
+  // 채널 목록 가져오기
+  const requestChannels = async (guildId: string): Promise<void> => {
     try {
-      const config = useRuntimeConfig();
-      const token = useCookie(config.public.accessTokenName);
-
-      await $fetch('/api/discord/send-message', {
+      const res = await $fetch<ChannelType[]>('/api/discord/channels', {
         method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token.value}`,
-        },
         query: {
           guildId,
-          channelId,
-          message,
-          isEveryone,
         },
       });
+      console.log('requestChannels111', res);
+      channelList.value = res;
+    } catch (e: any) {
+      console.error(e);
+    }
+  };
+
+  // 채널로 메시지 보내기
+  // todo channel.send를 못찾는다고 떠서 해결되어야 완성 가능
+  const sendMessage = async (params: SendMessageType): Promise<boolean> => {
+    try {
+      const res = await $fetch('/api/discord/send-message', {
+        method: 'POST',
+        body: JSON.stringify(params),
+      });
+      console.log('res : ', res);
       return true;
     } catch (e: any) {
       console.error(e);
@@ -38,12 +50,12 @@ export const useDiscordManageStore = defineStore('discordManage', () => {
   };
   const actions = {
     sendMessage,
+    requestChannels,
   };
 
   // ============= Return =============
   return {
     ...state,
-    ...getters,
     ...actions,
   };
 });
