7e7b2e856a733106054dc4efe3e4620ca59a896c
smw0807 <smw0807@gmail.com>
2024-05-12 18:55:47 +0900

    discord.js 라이브러리 안에 정보가 있어서 삭제

--- backend/src/constants/discord-api.ts
+++ backend/src/constants/discord-api.ts
@@ -24,22 +24,6 @@ export enum DISCORD_GRANT_TYPE {
   REFRESH_TOKEN = 'refresh_token',
 }
 
-// 디스코드 채널 타입
-export enum DISCORD_CHANNEL_TYPES {
-  GUILD_TEXT = 0,
-  DM = 1,
-  GUILD_VOICE = 2,
-  GROUP_DM = 3,
-  GUILD_CATEGORY = 4,
-  GUILD_ANNOUNCEMENT = 5,
-  ANNOUNCEMENT_THREAD = 10,
-  PUBLIC_THREAD = 11,
-  PRIVATE_THREAD = 12,
-  GUILD_STAGE_VOICE = 13,
-  GUILD_DIRECTORY = 14,
-  GUILD_FORUM = 15,
-  GUILD_MEDIA = 16,
-}
 /**
  * GUILDS
  * https://discord.com/developers/docs/activities/building-an-activity#step-7-use-the-api-to-fetch-the-guild
