d8f787963c938c896494ef2287ad8a18fdbc16cd
smw0807 <smw0807@gmail.com>
2024-05-16 21:15:34 +0900

    에러페이지 및 에러페이지용 레이아웃

---
+++ frontend/pages/error/[code].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'error',
+});
+const route = useRoute();
+</script>
+<template>
+  <div>
+    <h1>에러 페이지</h1>
+    <p>에러 코드: {{ route.params.code }}</p>
+    <v-btn @click="$router.back()">뒤로 가기</v-btn>
+  </div>
+</template>
