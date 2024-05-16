0d2a210e6381b8f1fa6b53d7f90f5aa38441abc1
smw0807 <smw0807@gmail.com>
2024-05-16 21:32:57 +0900

    에러페이지 수정 뒤로가기 -> 홈으로

--- frontend/pages/error/[code].vue
+++ frontend/pages/error/[code].vue
@@ -8,6 +8,6 @@ const route = useRoute();
   <div>
     <h1>에러 페이지</h1>
     <p>에러 코드: {{ route.params.code }}</p>
-    <v-btn @click="$router.back()">뒤로 가기</v-btn>
+    <v-btn @click="$router.push('/')">홈으로</v-btn>
   </div>
 </template>
