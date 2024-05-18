d87e9adb2a6beaeecc39632caf07c69d60abdb46
smw0807 <smw0807@gmail.com>
2024-05-18 16:34:07 +0900

    디스코드 사용자 서비스 추가

--- backend/src/discord/discord.module.ts
+++ backend/src/discord/discord.module.ts
@@ -9,6 +9,7 @@ import { DiscordController } from './discord.controller';
 import { DiscordGuildsService } from './guilds/discord.guilds.service';
 import { DiscordChannelService } from './guilds/discord.channel.service';
 import { SupabaseModule } from 'src/supabase/supabase.module';
+import { DiscordUsersService } from './users/discord.users.service';
 
 @Module({
   providers: [
@@ -18,6 +19,7 @@ import { SupabaseModule } from 'src/supabase/supabase.module';
     DiscordMessageService,
     DiscordGuildsService,
     DiscordChannelService,
+    DiscordUsersService,
   ],
   imports: [GoogleTranslateModule, HttpModule, SupabaseModule],
   controllers: [DiscordController],
