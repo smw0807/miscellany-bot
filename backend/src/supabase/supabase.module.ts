61a5e181d0143fe540f577236c2d563ed48c5ef2
smw0807 <smw0807@gmail.com>
2024-05-18 16:33:57 +0900

    메시지 히스토리 서비스 추가

--- backend/src/supabase/supabase.module.ts
+++ backend/src/supabase/supabase.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
+import { SendMessagesHistoryService } from './send-messages-history/msg.history.service';
 
 @Module({
-  providers: [SupabaseService],
-  exports: [SupabaseService],
+  providers: [SupabaseService, SendMessagesHistoryService],
+  exports: [SendMessagesHistoryService],
 })
 export class SupabaseModule {}
