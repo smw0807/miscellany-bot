383d755f864ed45eb3b4567932fc93c1dc675fe9
smw0807 <smw0807@gmail.com>
2024-05-18 17:06:13 +0900

    import prismaModule

--- backend/src/supabase/supabase.module.ts
+++ backend/src/supabase/supabase.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
 import { SendMessagesHistoryService } from './send-messages-history/msg.history.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [SupabaseService, SendMessagesHistoryService],
   exports: [SendMessagesHistoryService],
+  imports: [PrismaModule],
 })
 export class SupabaseModule {}
