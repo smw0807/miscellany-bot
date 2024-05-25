c3f1654fc7a23855a3a2f1968babc0e61a3ffea6
smw0807 <smw0807@gmail.com>
2024-05-25 18:25:21 +0900

    Alert, Confirm 컴포넌트에 사용할 아이콘 컴포넌트

---
+++ frontend/components/dialog/Icon/DialogIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ResultEnum } from '~/types/enums';
+const props = defineProps<{ type: ResultEnum; size?: string }>();
+const icon = computed(() => {
+  if (props?.type === 'success') {
+    return 'mdi-check-circle';
+  } else if (props?.type === 'error') {
+    return 'mdi-alert-circle';
+  } else if (props?.type === 'info') {
+    return 'mdi-information-variant-circle';
+  } else if (props?.type === 'warning') {
+    return 'mdi-alert';
+  }
+});
+</script>
+<template>
+  <v-icon
+    class="mb-5"
+    :color="props?.type"
+    :icon="icon"
+    :size="props.size ?? '100'"
+  ></v-icon>
+</template>
