2e37bc179f5cdc5953f7d60798d96f4fe90c366a
smw0807 <smw0807@gmail.com>
2024-05-26 19:35:04 +0900

    메시지 전송 내역 조회 로직

--- backend/src/supabase/send-messages-history/msg.history.service.ts
+++ backend/src/supabase/send-messages-history/msg.history.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { SendMessagesHistoryType } from 'src/discord/types/messages';
 import { PrismaService } from 'src/prisma/prisma.service';
 /**
@@ -13,10 +13,42 @@ export class SendMessagesHistoryService {
   async saveSendMessageHistory(data: SendMessagesHistoryType) {
     try {
       const result = await this.prisma.channelMessage.create({ data: data });
-      this.logger.log(result, '메시지 전송 내역 저장 성공');
+      this.logger.debug(result, '메시지 전송 내역 저장 성공');
     } catch (e) {
       console.error(e);
       this.logger.error('메시지 전송 내역 저장에 실패했습니다.', e);
+    }
+  }
+
+  //메시지 전송 내역 조회
+  async findSendMessageHistory(
+    guildId: string,
+    pageSzie: number,
+    pageIndex: number,
+  ) {
+    try {
+      const result = await this.prisma.channelMessage.findMany({
+        where: {
+          guildId: guildId,
+        },
+        take: pageSzie,
+        skip: pageIndex * pageSzie,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      this.logger.debug(result, '메시지 전송 내역 조회 성공');
+      return result;
+    } catch (e) {
+      this.logger.error('메시지 전송 내역 조회에 실패했습니다.', e);
+      if (e instanceof HttpException) {
+        throw new HttpException(
+          '메시지 전송 내역 조회에 실패했습니다.',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      } else {
+        throw e;
+      }
     }
   }
 }
