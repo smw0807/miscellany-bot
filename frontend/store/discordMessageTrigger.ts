8620de5db2e8ad47709e6af126aa10f70ff68dff
smw0807 <smw0807@gmail.com>
2024-05-30 21:45:50 +0900

    메시지 트리거 상태 저장소

---
+++ frontend/store/discordMessageTrigger.ts
@@ -0,0 +1,16 @@
+export const useDiscordMessagesTriggerStore = defineStore(
+  'discordMessagesTrigger',
+  () => {
+    // ============= State =============
+    const state = {};
+
+    // ============= Actions =============
+    const actions = {};
+
+    // ============= Return =============
+    return {
+      state,
+      actions,
+    };
+  }
+);
