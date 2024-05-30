1869d7c77cf8af5b63487ef6ec9260844e1f6e2e
smw0807 <smw0807@gmail.com>
2024-05-30 21:09:51 +0900

    alert 용 컴포넌트 디렉터리와 메시지 보내기 alert 컴포넌트

--- frontend/pages/manage/sendMessage.vue
+++ frontend/pages/manage/sendMessage.vue
@@ -93,21 +93,7 @@ onMounted(() => {
   <v-card>
     <v-card-title> [{{ guild.name }}] 서버에 메시지 보내기 </v-card-title>
     <v-card-text>
-      <v-alert dense border="start" border-color="info" class="mt-2">
-        <v-row align="center">
-          <v-col>
-            <span>
-              <strong>메시지 전송 안내</strong>
-              <br />
-              - 메시지는 5000자 이하로 입력해주세요.
-              <br />
-              - @everyone 적용 시 모든 유저에게 알림이 전송됩니다.
-              <br />
-              - 컨트롤+엔터를 누르면 바로 메시지를 보낼 수 있습니다.
-            </span>
-          </v-col>
-        </v-row>
-      </v-alert>
+      <alerts-send-message />
       <v-form ref="form">
         <v-row justify="center" align="center">
           <v-col cols="5" ms="12">
