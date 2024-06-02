35e695f803a52baf274813d8de4402701dcd4780
smw0807 <smw0807@gmail.com>
2024-06-03 01:10:56 +0900

    알림 컴포넌트 문구 제거

--- frontend/components/alerts/SendMessage.vue
+++ frontend/components/alerts/SendMessage.vue
@@ -3,8 +3,6 @@
     <v-row align="center">
       <v-col>
         <span>
-          <strong>메시지 전송 안내</strong>
-          <br />
           - 메시지는 5000자 이하로 입력해주세요.
           <br />
           - @everyone 적용 시 모든 유저에게 알림이 전송됩니다.
--- frontend/components/alerts/TriggerMessage.vue
+++ frontend/components/alerts/TriggerMessage.vue
@@ -3,8 +3,6 @@
     <v-row>
       <v-col class="text-left">
         <span>
-          <strong>메시지 트리거 관리 안내</strong>
-          <br />
           - 메시지는 1000자 이하로 입력해주세요.
           <br />
           - 특정 단어 입력 시 발송할 메시지를 작성해주세요.
