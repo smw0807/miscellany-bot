a4e4c3420714916ebcd9998a047c08234c18acdf
smw0807 <smw0807@gmail.com>
2024-06-02 17:53:23 +0900

    트리거 메시지 알림 컴포넌트

---
+++ frontend/components/alerts/TriggerMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert dense border="start" border-color="info" class="mt-2">
+    <v-row>
+      <v-col class="text-left">
+        <span>
+          <strong>메시지 트리거 관리 안내</strong>
+          <br />
+          - 메시지는 1000자 이하로 입력해주세요.
+          <br />
+          - 특정 단어 입력 시 발송할 메시지를 작성해주세요.
+          <br />
+          - @everyone 적용 시 모든 유저에게 알림이 전송됩니다.
+        </span>
+      </v-col>
+    </v-row>
+  </v-alert>
+</template>
