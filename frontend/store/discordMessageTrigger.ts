eef918b6c00ed95aee07d52cd8d184a4f8f8a443
smw0807 <smw0807@gmail.com>
2024-06-04 22:14:43 +0900

    트리거메시지 등록 구현 준비

--- frontend/store/discordMessageTrigger.ts
+++ frontend/store/discordMessageTrigger.ts
@@ -7,17 +7,37 @@ export type TriggerMessageType = {
 export const useDiscordMessagesTriggerStore = defineStore(
   'discordMessagesTrigger',
   () => {
+    const { useAlert, useConfirm } = useDialog();
     // ============= State =============
 
     const state = {};
 
     // ============= Actions =============
-    const actions = {};
+    // 트리거 메시지 목록 조회
+    // 트리거 메시지 등록
+    const addTriggerMessage = async (
+      params: TriggerMessageType
+    ): Promise<void> => {
+      try {
+        const res = await $fetch('/api/discord/trigger-message', {
+          method: 'POST',
+          params,
+        });
+        console.log('res : ', res);
+      } catch (e) {
+        // const error: NestHttpException = e;
+        console.error(e);
+      }
+    };
+    // 트리거 메시지 수정
+
+    // 트리거 메시지 삭제
+    const actions = { addTriggerMessage };
 
     // ============= Return =============
     return {
-      state,
-      actions,
+      ...state,
+      ...actions,
     };
   }
 );
