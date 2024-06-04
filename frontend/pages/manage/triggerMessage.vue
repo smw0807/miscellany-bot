461f86c62efff00a88ed82d88bd0f1075bca675f
smw0807 <smw0807@gmail.com>
2024-06-04 22:14:28 +0900

    트리거메시지 등록 스토어로 요청

--- frontend/pages/manage/triggerMessage.vue
+++ frontend/pages/manage/triggerMessage.vue
@@ -1,6 +1,42 @@
 <script setup lang="ts">
+import AddTrigger from '~/components/dialog/AddTrigger.vue';
+import type { TriggerMessageType } from '~/store/discordMessageTrigger';
+import { useDiscordMessagesTriggerStore } from '~/store/discordMessageTrigger';
 definePageMeta({
   layout: 'manage',
 });
+
+const triggerStore = useDiscordMessagesTriggerStore();
+
+const openAddTriggerDialog = ref(false);
+
+// 트리거 추가 다이얼로그 열기
+const openDialog = () => {
+  openAddTriggerDialog.value = true;
+};
+// 트리거 추가 다이얼로그 닫기
+const closeDialog = () => {
+  openAddTriggerDialog.value = false;
+};
+// 트리거 저장
+const saveTrigger = async (data: TriggerMessageType) => {
+  await triggerStore.addTriggerMessage(data);
+};
 </script>
-<template></template>
+<template>
+  <v-card>
+    <v-card-title> 메시지 트리거 관리 </v-card-title>
+    <v-card-text>
+      <alerts-trigger-message />
+      <div class="text-right mt-3">
+        <v-btn color="warning" @click="openDialog"> 트리거 추가 </v-btn>
+      </div>
+    </v-card-text>
+    <v-card-text> Table Area </v-card-text>
+  </v-card>
+  <AddTrigger
+    :open="openAddTriggerDialog"
+    @input-data="saveTrigger"
+    @onClose="closeDialog"
+  />
+</template>
