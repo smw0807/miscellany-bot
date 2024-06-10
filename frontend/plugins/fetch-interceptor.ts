1ab3a76bf0c2f149c93592b49d8f076a4e2f4ed1
smw0807 <smw0807@gmail.com>
2024-06-11 07:38:59 +0900

    500에러시 알림창 출력

--- frontend/plugins/fetch-interceptor.ts
+++ frontend/plugins/fetch-interceptor.ts
@@ -1,5 +1,7 @@
 //https://stackoverflow.com/questions/75434573/redefine-fetch-in-nuxt3-with-global-onrequest-handler
 import { ofetch } from 'ofetch';
+import useDialog from '~/composables/useDialog';
+import { ResultTypeEnum } from '~/types/enums';
 export default defineNuxtPlugin((nuxtApp) => {
   globalThis.$fetch = ofetch.create({
     onRequest({ _request, options }) {
@@ -12,6 +14,15 @@ export default defineNuxtPlugin((nuxtApp) => {
     onResponse({ response }) {
       // console.log('onResponse', response);
       const status = response.status;
+      if (status === 500) {
+        const { useAlert } = useDialog();
+        useAlert({
+          type: ResultTypeEnum.ERROR,
+          title: '서버 에러',
+          message:
+            '서버 에러가 발생했습니다.<br/>잠시 후 다시 시도해주세요.<br/>문제가 지속될 경우 관리자에게 문의해주세요.',
+        });
+      }
       if (status === 401) {
         const token = useAuth();
         token.clearToken();
