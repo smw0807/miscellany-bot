1d764dec36f0c9dc1f94d8efb46ab4f78bd35286
smw0807 <smw0807@gmail.com>
2024-06-24 20:35:10 +0900

    EditScheduleMessage Alert 컴포넌트 추가

---
+++ frontend/components/alerts/EditScheduleMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert dense border="start" border-color="info" class="mt-2">
+    <v-row>
+      <v-col class="text-left">
+        <span>
+          - 특정 메시지 발송을 예약할 수 있습니다.
+          <br />
+          - 메시지는 정해진 시간에 한 번 또는 반복적으로 발송할 수 있습니다.
+          <br />
+          - @everyone 적용 시 모든 유저에게 알림이 전송됩니다.
+          <br />
+          - 메시지는 1000자 이하로 입력해주세요.
+        </span>
+      </v-col>
+    </v-row>
+  </v-alert>
+</template>
