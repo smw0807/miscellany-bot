6d6dcb48db0c799f195c1bb49d1bd0239d0dd705
smw0807 <smw0807@gmail.com>
2024-06-24 19:46:20 +0900

    예약 메시지 저장소

---
+++ frontend/store/discordSchedule.ts
@@ -0,0 +1,13 @@
+export const useDiscordScheduleStore = defineStore('discordSchedule', () => {
+  // ============= State =============
+  const state = {};
+
+  // ============= Actions =============
+  const asctions = {};
+
+  // ============= Returns =============
+  return {
+    ...state,
+    ...asctions,
+  };
+});
