1e44f3523bd34e0be8e69ea1f9f5ef2f5f492ede
smw0807 <smw0807@gmail.com>
2024-06-24 19:43:11 +0900

    예약메시지 Alert 컴포넌트 추가 및 적용

---
+++ frontend/components/alerts/ScheduleMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-alert dense border="start" border-color="info" class="mt-2">
+    <v-row>
+      <v-col class="text-left">
+        <span>
+          - 특정 메시지 발송을 예약할 수 있습니다.
+          <br />
+          - 메시지를 정해진 시간에 한 번 또는 반복적으로 발송할 수 있습니다.
+          <br />
+          - 체크박스를 통해 등록된 예약 메시지를 제거할 수 있습니다.
+        </span>
+      </v-col>
+    </v-row>
+  </v-alert>
+</template>
--- frontend/pages/manage/scheduleMessage.vue
+++ frontend/pages/manage/scheduleMessage.vue
@@ -8,6 +8,7 @@ const totalItems = ref(0);
   <v-card>
     <v-card-title> 예약 메시지 관리 </v-card-title>
     <v-card-text>
+      <alerts-schedule-message />
       <div class="text-right mt-3">
         <v-btn color="warning"> 예약 메시지 추가 </v-btn>
         <v-btn color="red" class="ml-1"> 예약 메시지 삭제</v-btn>
@@ -15,7 +16,7 @@ const totalItems = ref(0);
     </v-card-text>
 
     <v-card-title class="d-flex">
-      <span>등록된 트리거</span>
+      <span>등록된 예약 메시지</span>
       <v-spacer />
       <div class="text-subtitle-1 text-">총 {{ totalItems }} 개</div>
     </v-card-title>
