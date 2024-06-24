d64fd04a0ea1d0386b0eff6219b31bcba0a3f795
smw0807 <smw0807@gmail.com>
2024-06-25 07:21:46 +0900

    예약메시지 타입 및 상태값 추가

--- frontend/store/discordSchedule.ts
+++ frontend/store/discordSchedule.ts
@@ -1,6 +1,36 @@
+import type { ScheduleType } from '~/types/enums';
+
+// 예약 메시지 데이터 타입
+export type ScheduleMessageType = {
+  id?: number;
+  guildId: String;
+  channelId: String;
+  scheduleType: ScheduleType;
+  title: String;
+  messageContent: String;
+  scheduledAt?: Date;
+  repeatInterval?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  createdBy?: String;
+  isActive?: Boolean;
+  lastSentAt?: Date;
+};
+
 export const useDiscordScheduleStore = defineStore('discordSchedule', () => {
   // ============= State =============
-  const state = {};
+  const guildId = ref<string>(''); // 길드 아이디
+  const pageIndex = ref<number>(1); // 페이지 인덱스
+  const pageSize = ref<number>(10); // 페이지 사이즈
+  const total = ref<number>(0); // 전체 데이터 수
+  const scheduleMessages = ref<ScheduleMessageType[]>([]); // 예약 메시지
+  const state = {
+    guildId,
+    pageIndex,
+    pageSize,
+    total,
+    scheduleMessages,
+  };
 
   // ============= Actions =============
   const asctions = {};
