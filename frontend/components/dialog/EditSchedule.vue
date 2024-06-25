47e9508443d808b564de8580f4799487811ebff0
smw0807 <smw0807@gmail.com>
2024-06-26 07:27:35 +0900

    예약메시지 입력 폼

--- frontend/components/dialog/EditSchedule.vue
+++ frontend/components/dialog/EditSchedule.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { EditTypeEnum, ScheduleType } from '~/types/enums';
 import type { ChannelType } from '~/store/discordManage';
+import type { ScheduleMessageType } from '~/store/discordSchedule';
 const props = withDefaults(
   defineProps<{
     open: boolean;
@@ -13,8 +14,17 @@ const props = withDefaults(
   }
 );
 const emit = defineEmits({
+  'input-data': (mode: EditTypeEnum, data: ScheduleMessageType) => true,
   onClose: () => true,
 });
+
+const cChannels = computed(() => props.channels);
+watch(
+  () => cChannels,
+  (newVal) => {
+    console.log(newVal);
+  }
+);
 
 const form = ref();
 // @everyone 여부
@@ -27,6 +37,12 @@ const selectedChannel = ref<ChannelType>();
 const title = ref<string>('');
 // 예약 메시지 내용
 const message = ref<string>('');
+
+const emitSave = async () => {
+  const { valid } = await form.value.validate();
+  if (!valid) return;
+  console.log('emitSave');
+};
 </script>
 <template>
   <v-sheet>
@@ -42,27 +58,35 @@ const message = ref<string>('');
           예약 메시지 {{ props.mode === 'add' ? '추가' : '수정' }}
         </h2>
         <alerts-edit-schedule-message />
+        <v-divider class="mt-4" />
         <v-form ref="form">
-          <!-- @everyone 여부 -->
-          <v-checkbox
-            v-model="isEveryone"
-            label="@everyone 적용"
-            hide-details
-          ></v-checkbox>
-
-          <!-- 예약 메시지 타입 -->
-          <v-radio-group v-model="scheduleType" inline hide-details>
-            <v-radio label="1번 전송" :value="ScheduleType.ONETIME"></v-radio>
-            <v-radio
-              label="반복 전송"
-              :value="ScheduleType.RECURRING"
-            ></v-radio>
-          </v-radio-group>
-
+          <v-row align="center">
+            <v-col md="6" xs="12">
+              <!-- @everyone 여부 -->
+              <v-checkbox
+                v-model="isEveryone"
+                label="@everyone 적용"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+            <v-col md="6" xs="12">
+              <!-- 예약 메시지 타입 -->
+              <v-radio-group v-model="scheduleType" inline hide-details>
+                <v-radio
+                  label="1번 전송"
+                  :value="ScheduleType.ONETIME"
+                ></v-radio>
+                <v-radio
+                  label="반복 전송"
+                  :value="ScheduleType.RECURRING"
+                ></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
           <!-- 예약 메시지 전송 채널 -->
           <v-select
             v-model="selectedChannel"
-            :items="props.channels"
+            :items="cChannels"
             label="예약 메시지 발송할 채널"
             item-text="name"
             item-value="id"
@@ -90,7 +114,12 @@ const message = ref<string>('');
         <v-divider class="mb-4"></v-divider>
 
         <div class="text-end">
-          <v-btn class="text-none" color="primary" variant="flat" rounded
+          <v-btn
+            class="text-none"
+            color="primary"
+            variant="flat"
+            rounded
+            @clic="emitSave"
             >저장</v-btn
           >
           <v-btn rounded @click="emit('onClose')">닫기</v-btn>
