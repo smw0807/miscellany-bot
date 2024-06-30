745d81fd865553c173c7fee1eff56ea83377df56
smw0807 <smw0807@gmail.com>
2024-06-30 21:19:26 +0900

    뷰티파이 추가 컴포넌트 적용

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,11 +1,16 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import { VDateInput, VTimePicker } from 'vuetify/labs/components';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     theme: {
       defaultTheme: 'dark',
+    },
+    components: {
+      VDateInput,
+      VTimePicker,
     },
   });
   app.vueApp.use(vuetify);
