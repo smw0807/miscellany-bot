679486792694ca78e530069cf6c09e064167c4f9
smw0807 <smw0807@gmail.com>
2024-06-30 22:55:55 +0900

    TimePicker컴포넌트 추가 여러 컴포넌트 조합으로 구성되어 있어서 컴포넌트 파일로 만듦

---
+++ frontend/components/time/TimePicker.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    time: string;
+    open: boolean;
+    label: string;
+  }>(),
+  {
+    time: '',
+    open: false,
+    label: 'Time',
+  }
+);
+const emit = defineEmits(['close']);
+
+const open = ref(props.open);
+const time = ref(props.time);
+</script>
+<template>
+  <v-text-field v-model="time" :active="open" :label="props.label" readonly>
+    <v-menu
+      v-model="open"
+      :close-on-content-click="false"
+      activator="parent"
+      transition="scale-transition"
+    >
+      <v-time-picker
+        v-if="open"
+        v-model="time"
+        color="green-lighten-1"
+        full-width
+      >
+      </v-time-picker>
+    </v-menu>
+  </v-text-field>
+</template>
