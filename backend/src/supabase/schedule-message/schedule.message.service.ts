457cd32a59b8d8add8c80e0a19193a7d2bfbcaf6
송민우 <smw0807@gmail.com>
2024-07-04 16:37:41 +0900

    예약 메시지 등록 함수

--- backend/src/supabase/schedule-message/schedule.message.service.ts
+++ backend/src/supabase/schedule-message/schedule.message.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { ScheduledMessage } from '@prisma/client';
+import { ScheduledMessage, ScheduleType } from '@prisma/client';
 
 @Injectable()
 export class ScheduleMessageService {
@@ -10,13 +10,22 @@ export class ScheduleMessageService {
   // 예약 메시지 등록
   async addScheduleMessage(data: ScheduledMessage) {
     try {
+      if (!ScheduleType[data.scheduleType as keyof typeof ScheduleType]) {
+        throw new HttpException(
+          '예약 타입이 잘못되었습니다.',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
       const result = await this.prisma.scheduledMessage.create({
-        data,
+        data: {
+          ...data,
+          scheduledAt: new Date(data.scheduledAt),
+        },
       });
       this.logger.debug(result, '예약 메시지 등록 성공');
       return HttpStatus.OK;
     } catch (e) {
-      this.logger.error('예약 메시지 등록 실패', e);
+      this.logger.error('예약 메시지 등록 실패', e.message);
       throw new HttpException(
         '예약 메시지 등록에 실패했습니다.',
         HttpStatus.INTERNAL_SERVER_ERROR,
