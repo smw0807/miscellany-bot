f1089596f9ebdb07c67b2f6eea811dbc29e7d8d2
smw0807 <smw0807@gmail.com>
2024-07-20 17:32:22 +0900

    예약메시지 실패처리 로직 추가

--- backend/src/supabase/schedule-message/schedule.job.service.ts
+++ backend/src/supabase/schedule-message/schedule.job.service.ts
@@ -11,6 +11,9 @@ export class ScheduleMessageJobService implements OnModuleInit {
   ) {}
   onModuleInit() {
     this.logger.warn('ScheduleMessageJobService has been initialized.');
+    // 발송안된 1회성 예약 메시지 FAIL 처리
+    this.failOneTimeMessages();
+    // 처리할 예약 메시지들 처리 시작
     this.getScheduleMessages();
   }
   onModuleDestroy() {
@@ -34,11 +37,35 @@ export class ScheduleMessageJobService implements OnModuleInit {
     // 등록되어 있는 크론잡 목록
     const jobs = this.schedulerRegistry.getCronJobs();
     jobs.forEach((value, key) => {
-      const nextDate = value.nextDates().toDate();
+      const nextDate = value.nextDates();
       this.logger.log(`CronJob: ${key} -> Next Date: ${nextDate}`);
     });
   }
   //============= List ============= E
+
+  //============= ETC ============== S
+  // 아직 발송안된 1회성 메시지 FAIL 처리
+  async failOneTimeMessages() {
+    try {
+      const oneTimeMessages = await this.prisma.scheduledMessage.findMany({
+        where: { scheduleType: 'ONETIME', scheduledAt: { lt: new Date() } },
+        select: { id: true },
+      });
+      const updated = await this.prisma.scheduledMessage.updateMany({
+        where: { id: { in: oneTimeMessages.map((m) => m.id) } },
+        data: { sendStatus: 'FAIL' },
+      });
+      this.logger.log(
+        `1회성 메시지 중에 아직 전송 안된 메시지 ${updated.count}개 FAIL 처리`,
+      );
+    } catch (e) {
+      this.logger.error(
+        '1회성 메시지 중에 아직 전송 안된 메시지 FAIL 처리에 실패했습니다.',
+        e.message,
+      );
+    }
+  }
+  //============= ETC ============== E
 
   // 크론잡 삭제
   async deleteCronJob() {
@@ -58,7 +85,11 @@ export class ScheduleMessageJobService implements OnModuleInit {
   // 데이터베이스에 있는 예약메시지 데이터 가져와서 스케줄 등록하기
   async getScheduleMessages() {
     try {
-      const scheduledMessages = await this.prisma.scheduledMessage.findMany();
+      // 현재 시간보다 큰 예약메시지 데이터 가져오기
+      const scheduledMessages = await this.prisma.scheduledMessage.findMany({
+        where: { scheduledAt: { gte: new Date() } },
+      });
+
       for (const message of scheduledMessages) {
         if (message.scheduleType === 'ONETIME') {
           this.logger.log(message, 'ONETIME Message');
