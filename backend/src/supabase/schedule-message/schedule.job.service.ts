7aa19a154aef1af215fc33072817acdfbe226cae
smw0807 <smw0807@gmail.com>
2024-07-31 22:48:28 +0900

    소스 정리

--- backend/src/supabase/schedule-message/schedule.job.service.ts
+++ backend/src/supabase/schedule-message/schedule.job.service.ts
@@ -6,7 +6,7 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 import { ScheduleMessageService } from './schedule.message.service';
 import { RepeatType, ScheduledMessage } from '@prisma/client';
@@ -21,7 +21,6 @@ export class ScheduleMessageJobService implements OnModuleInit {
     private readonly prisma: PrismaService,
     private schedulerRegistry: SchedulerRegistry,
     @Inject(forwardRef(() => ScheduleMessageService))
-    private readonly scheduleService: ScheduleMessageService,
     private readonly discordMessageService: DiscordMessageService,
   ) {}
   async onModuleInit() {
@@ -34,107 +33,6 @@ export class ScheduleMessageJobService implements OnModuleInit {
   onModuleDestroy() {
     this.logger.warn('ScheduleMessageJobService has been destroyed.');
   }
-
-  //============= Check ============= S
-  // 크론잡 등록되어 있는지 확인
-  private async checkCronJob(jobName: string) {
-    // 크론잡 등록되어 있는지 확인
-    try {
-      const job = this.schedulerRegistry.getCronJob(jobName);
-      this.logger.debug('### 크론잡이 등록되어 있습니다.', jobName);
-      this.logger.debug(job);
-      return true;
-    } catch (e) {
-      this.logger.debug('### 크론잡이 등록되어 있지 않습니다.', jobName);
-      return false;
-    }
-  }
-  //============= Check ============= E
-
-  //============= List ============= S
-  // 등록되어 있는 목록
-  private listCronJobs() {
-    // 등록되어 있는 크론잡 목록
-    const jobs = this.schedulerRegistry.getCronJobs();
-    this.logger.log('============ [ CronJob List ] ============');
-    jobs.forEach((value, key) => {
-      const nextDate = value.nextDates();
-      this.logger.log(`CronJob: ${key} -> Next Date: ${nextDate}`);
-    });
-    this.logger.log('=========================================');
-  }
-  //============= List ============= E
-
-  //============ CronJob ============ S
-  // 크론잡 등록
-  async addCronJob(
-    jobName: string,
-    date: Date,
-    data: Partial<ScheduledMessage>,
-  ) {
-    if (await this.checkCronJob(jobName)) {
-      return;
-    }
-    const cronTime =
-      data.scheduleType === 'ONETIME'
-        ? date
-        : this.makeCronTime(date, data.repeatInterval, data.repeatType);
-    const job = new CronJob(cronTime, async () => {
-      // 디스코드 메시지 발송
-      const sendMessage: SendMessageType = {
-        guildId: data.guildId,
-        channelId: data.channelId,
-        isEveryone: data.isEveryone,
-        message: data.messageContent,
-      };
-
-      await this.discordMessageService.snedScheduleMessage(
-        data.id,
-        sendMessage,
-      );
-      if (data.scheduleType === 'ONETIME') {
-        // 전달 받은 시간에 디스코드 메시지 발 송 후 크론잡 삭제
-        this.schedulerRegistry.deleteCronJob(jobName);
-      }
-    });
-    this.schedulerRegistry.addCronJob(jobName, job);
-    job.start();
-    this.logger.log('크론잡 등록');
-    this.listCronJobs();
-  }
-  // 반복 메시지 크론잡 시간 설정
-  private makeCronTime(
-    date: Date,
-    repeatInterval: number,
-    repeatType: RepeatType,
-  ) {
-    const d = dayjs(date);
-    if (repeatType === 'DAY') {
-      return `0 ${d.minute()} ${d.hour()} * * *`;
-    } else if (repeatType === 'HOUR') {
-      return `0 ${d.minute()} * * * *`;
-    } else if (repeatType === 'MINUTE') {
-      return `${d.minute()}/${repeatInterval} ${d.hour()}-23 * * *`;
-    }
-  }
-
-  // 크론잡 삭제
-  async deleteCronJob(jobName: string) {
-    try {
-      const check = await this.checkCronJob(jobName);
-      if (!check) {
-        return;
-      }
-      const job = this.schedulerRegistry.getCronJob(jobName);
-      job.stop();
-      this.schedulerRegistry.deleteCronJob(jobName);
-      this.logger.log('크론잡 삭제 성공', jobName);
-      this.listCronJobs();
-    } catch (e) {
-      this.logger.error('크론잡 삭제 실패', e.message);
-    }
-  }
-  //============ CronJob ============ E
 
   // 데이터베이스에 있는 예약메시지 데이터 가져와서 스케줄 등록하기
   private async initMakeScheduleMessages() {
@@ -172,6 +70,7 @@ export class ScheduleMessageJobService implements OnModuleInit {
             message,
           );
         } else {
+          // 현재 시간보다 큰 경우 다음 시작 시간 계산해서 크론잡 등록
           const nextScheduledAt = await this.makeCronJobNextStartTime(
             new Date(now),
             new Date(scheduledAt),
@@ -189,6 +88,97 @@ export class ScheduleMessageJobService implements OnModuleInit {
       this.logger.error('예약 메시지 스케줄 등록에 실패했습니다.', e.message);
     }
   }
+
+  //============= List ============= S
+  // 등록되어 있는 목록
+  private listCronJobs() {
+    // 등록되어 있는 크론잡 목록
+    const jobs = this.schedulerRegistry.getCronJobs();
+    this.logger.log('============ [ CronJob List ] ============');
+    jobs.forEach((value, key) => {
+      const nextDate = value.nextDates();
+      this.logger.log(`CronJob: ${key} -> Next Date: ${nextDate}`);
+    });
+    this.logger.log('=========================================');
+  }
+  //============= List ============= E
+
+  //============= Check ============= S
+  // 크론잡 등록되어 있는지 확인
+  private async checkCronJob(jobName: string) {
+    // 크론잡 등록되어 있는지 확인
+    try {
+      const job = this.schedulerRegistry.getCronJob(jobName);
+      this.logger.debug('### 크론잡이 등록되어 있습니다.', jobName);
+      this.logger.debug(job);
+      return true;
+    } catch (e) {
+      this.logger.debug('### 크론잡이 등록되어 있지 않습니다.', jobName);
+      return false;
+    }
+  }
+  //============= Check ============= E
+
+  //============ CronJob ============ S
+  // 크론잡 등록
+  async addCronJob(
+    jobName: string,
+    date: Date,
+    data: Partial<ScheduledMessage>,
+  ) {
+    // 이미 등록되어 있는 크론잡인지 확인
+    if (await this.checkCronJob(jobName)) {
+      return;
+    }
+    // 크론잡 시간 설정
+    const cronTime =
+      data.scheduleType === 'ONETIME'
+        ? date
+        : this.makeCronTime(date, data.repeatInterval, data.repeatType);
+
+    // 크론잡 등록
+    const job = new CronJob(cronTime, async () => {
+      // 디스코드 메시지 발송
+      const sendMessage: SendMessageType = {
+        guildId: data.guildId,
+        channelId: data.channelId,
+        isEveryone: data.isEveryone,
+        message: data.messageContent,
+      };
+
+      await this.discordMessageService.snedScheduleMessage(
+        data.id,
+        sendMessage,
+      );
+      if (data.scheduleType === 'ONETIME') {
+        // 전달 받은 시간에 디스코드 메시지 발 송 후 크론잡 삭제
+        this.schedulerRegistry.deleteCronJob(jobName);
+      }
+    });
+    this.schedulerRegistry.addCronJob(jobName, job);
+    job.start();
+    this.logger.log('크론잡 등록');
+    this.listCronJobs();
+  }
+
+  // 크론잡 삭제
+  async deleteCronJob(jobName: string) {
+    try {
+      // 크론잡 등록되어 있는지 확인
+      const check = await this.checkCronJob(jobName);
+      if (!check) {
+        return;
+      }
+      const job = this.schedulerRegistry.getCronJob(jobName);
+      job.stop();
+      this.schedulerRegistry.deleteCronJob(jobName);
+      this.logger.log('크론잡 삭제 성공', jobName);
+      this.listCronJobs();
+    } catch (e) {
+      this.logger.error('크론잡 삭제 실패', e.message);
+    }
+  }
+  //============ CronJob ============ E
 
   // 반복 메시지 시작 시간이 현재 시간보다 작을 경우 다음 시작 시간 계산
   private async makeCronJobNextStartTime(
@@ -217,5 +207,21 @@ export class ScheduleMessageJobService implements OnModuleInit {
       }
     }
     return nextScheduledAt.locale('ko').toDate();
+  }
+
+  // 반복 메시지 크론잡 시간 설정
+  private makeCronTime(
+    date: Date,
+    repeatInterval: number,
+    repeatType: RepeatType,
+  ) {
+    const d = dayjs(date);
+    if (repeatType === 'DAY') {
+      return `0 ${d.minute()} ${d.hour()} * * *`;
+    } else if (repeatType === 'HOUR') {
+      return `0 ${d.minute()} * * * *`;
+    } else if (repeatType === 'MINUTE') {
+      return `${d.minute()}/${repeatInterval} ${d.hour()}-23 * * *`;
+    }
   }
 }
