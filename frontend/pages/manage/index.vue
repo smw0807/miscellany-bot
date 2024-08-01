aa48761d1c046325e8259811de3d476236667f63
smw0807 <smw0807@gmail.com>
2024-08-01 21:05:16 +0900

    메인화면 설명 문구

--- frontend/pages/manage/index.vue
+++ frontend/pages/manage/index.vue
@@ -6,24 +6,34 @@ const config = useRuntimeConfig();
 const projectName: Ref<string> = ref(config.public.projectName);
 </script>
 <template>
-  <v-sheet
-    class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4"
-    elevation="4"
-    height="250"
-    max-width="800"
-    width="100%"
-    rounded
-  >
-    <div>
-      <h2 class="text-h4 font-weight-black text-orange">기능 구성 및 사용법</h2>
+  <v-sheet border="md" class="pa-6 text-white mx-auto" color="#141518">
+    <h4 class="text-h5 font-weight-bold mb-4">{{ projectName }}</h4>
 
-      <!-- <div class="text-h5 font-weight-medium mb-2">
-        현재 총 3가지 기능으로 구성되어 있습니다.
-      </div> -->
+    <p>
+      안녕하세요.
+      <br />
+      {{ projectName }}은 현재 4가지 기능을 제공하고 있습니다.
+      <br />
+      <li>디스코드 채팅 번역</li>
+      <li>채널로 메시지 보내기</li>
+      <li>트리거 메시지</li>
+      <li>메시지 전송 예약</li>
 
-      <p class="text-body-2 my-4">총 3가지 기능으로 구성되어 있습니다.</p>
+      <br />
 
-      <v-btn color="orange" variant="text">ㅇㅇ</v-btn>
-    </div>
+      1. 디스코드 <b class="text-red">채팅 번역 기능</b>은 이
+      <u>관리자 페이지에서 기능을 제공하지 않고</u>, 디스코드 자체에서
+      제공됩니다. 디스코드에서 번역하고 싶은 채팅 메시지를 우클릭 해서 [앱]
+      메뉴에서 번역하고 싶은 언어를 선택할 수 있습니다.
+      <br />
+      2. <b class="text-red">채널로 메시지 보내기</b>는 특정 채널로 메시지를
+      보낼 수 있는 기능입니다.
+      <br />
+      3. <b class="text-red">트리거 메시지</b>는 디스코드 채팅방에서 특정
+      키워드가 포함된 메시지를 받았을 때, 저장된 메시지를 보내는 기능입니다.
+      <br />
+      4. <b class="text-red">예약 메시지</b>는 특정 시간에 메시지를 보낼 수 있는
+      기능입니다. 1회성으로 전송할 수도 있고, 주기적으로 전송할 수도 있습니다.
+    </p>
   </v-sheet>
 </template>
