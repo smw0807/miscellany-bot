d1d1a4ab740fff86e449126558c90528e7bb72de
smw0807 <smw0807@gmail.com>
2024-08-07 23:27:48 +0900

    cachemodule 추가

---
+++ backend/src/caching/caching.module.ts
@@ -0,0 +1,10 @@
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { CachingService } from './cacing.service';
+
+@Module({
+  imports: [CacheModule.register()],
+  providers: [CachingService],
+  exports: [CachingService],
+})
+export class CachingModule {}
