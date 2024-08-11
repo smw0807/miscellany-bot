a1c75301570ce321da425ecb211fc5524a94653d
smw0807 <smw0807@gmail.com>
2024-08-11 19:43:37 +0900

    docker-compose 개발모드 추가

--- backend/run.sh
+++ backend/run.sh
@@ -7,7 +7,14 @@ if [ "$1" == "build" ]; then
     docker-compose -f docker-compose.yml build
 elif [ "$1" == "up" ]; then
     echo "Starting backend"
-    docker-compose -f docker-compose.yml up
+    if [ "$2" == "dev" ]; then
+        echo "Starting backend in development mode"
+        NODE_ENV=development docker-compose --env-file .env.dev -f docker-compose.yml  up -d
+    else
+        echo "Starting backend in production mode"
+        NODE_ENV=production docker-compose -f docker-compose.yml up -d
+    fi
+    #docker-compose -f docker-compose.yml up -d
 else
     echo "Invalid parameter"
 fi
