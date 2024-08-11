c2bd35784b5ce1498ec6db548742974766351a48
smw0807 <smw0807@gmail.com>
2024-08-11 14:02:30 +0900

    docker-compose 추가 및 빌드 실행 쉘 파일 추가

---
+++ backend/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Building backend"
+
+# build or up 파라미터 받기
+if [ "$1" == "build" ]; then
+    echo "Building backend"
+    docker-compose -f docker-compose.yml build
+elif [ "$1" == "up" ]; then
+    echo "Starting backend"
+    docker-compose -f docker-compose.yml up
+else
+    echo "Invalid parameter"
+fi
