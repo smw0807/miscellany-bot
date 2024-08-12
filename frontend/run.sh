d57d41732ff36e23b690323fae29bc899ffe2e2a
smw0807 <smw0807@gmail.com>
2024-08-12 23:03:43 +0900

    쉘 수정

--- frontend/run.sh
+++ frontend/run.sh
@@ -4,7 +4,7 @@ echo "Building frontend"
 # build or up 파라미터 받기
 if [ "$1" == "build" ]; then
     echo "Building frontend"
-    docker-compose -f docker-compose.yml build --no-cache
+    docker-compose -f docker-compose.yml build
 elif [ "$1" == "up" ]; then
     echo "Starting frontend"
     docker-compose -f docker-compose.yml up -d
