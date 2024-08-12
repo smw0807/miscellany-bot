815943deec97bc472292bcbbbb63278557f44855
smw0807 <smw0807@gmail.com>
2024-08-12 22:23:28 +0900

    쉘 수정

--- backend/run.sh
+++ backend/run.sh
@@ -7,14 +7,7 @@ if [ "$1" == "build" ]; then
     docker-compose -f docker-compose.yml build
 elif [ "$1" == "up" ]; then
     echo "Starting backend"
-    if [ "$2" == "dev" ]; then
-        echo "Starting backend in development mode"
-        NODE_ENV=development docker-compose --env-file .env.dev -f docker-compose.yml  up -d
-    else
-        echo "Starting backend in production mode"
-        NODE_ENV=production docker-compose -f docker-compose.yml up -d
-    fi
-    #docker-compose -f docker-compose.yml up -d
+    docker-compose -f docker-compose.yml up -d
 else
     echo "Invalid parameter"
 fi
