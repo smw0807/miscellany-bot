0a8fe49c4e2ca2b32fde41ffe4a0d2362b574364
smw0807 <smw0807@gmail.com>
2024-09-18 16:51:56 +0900

    원복

--- frontend/run.sh
+++ frontend/run.sh
@@ -4,8 +4,8 @@ echo "Building frontend"
 # build or up 파라미터 받기
 if [ "$1" == "build" ]; then
     echo "Building frontend"
-    #docker-compose -f docker-compose.yml build
-    docker buildx build --platform linux/amd64 -t miscellany-fe .
+    docker-compose -f docker-compose.yml build
+    #docker buildx build --platform linux/amd64 -t miscellany-fe .
 elif [ "$1" == "up" ]; then
     echo "Starting frontend"
     docker-compose -f docker-compose.yml up -d
