9e327b3dcc73c294ce1d2400f98b5f7a5d1deadf
smw0807 <smw0807@gmail.com>
2024-10-20 07:54:42 +0900

    확인용 로그

--- frontend/server/middleware/proxy.ts
+++ frontend/server/middleware/proxy.ts
@@ -1,9 +1,9 @@
 //https://jongmin4943.tistory.com/entry/Nuxt3-proxy-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0feat-Spring-API-%EC%84%9C%EB%B2%84
 export default defineEventHandler((event) => {
+  console.log('proxy', process.env.API_URL);
   if (!event.node.req.url?.startsWith('/api/')) return;
 
   const target = new URL(event.node.req.url, process.env.API_URL);
-  console.log('proxy', target.toString());
   return proxyRequest(event, target.toString(), {
     headers: {
       host: target.host,
